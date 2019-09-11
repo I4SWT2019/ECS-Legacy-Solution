44585dabfcd8d4777caac651240f11119c4a5d77
Winckler <au534747@post.au.dk>
2019-09-11 14:08:35 +0200

    Added Refactor.ECS with all classes according to design refactor

---
+++ ECS.Refactored/FakeTempSensor.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ECS.Refactored
+{
+    public class FakeTempSensor : ITempSensor
+    {
+        public int _temp;
+
+        public int GetTemp()
+        {
+            return _temp;
+        }
+
+        public bool RunSelfTest()
+        {
+            return true;
+        }
+    }
+}
