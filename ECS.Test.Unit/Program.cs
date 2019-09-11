3a6a3df67caf0177fb5fe8dbab34bef21e122eca
Winckler <au534747@post.au.dk>
2019-09-11 13:21:07 +0200

    using NUnit.frame...

--- ECS.Test.Unit/Program.cs
+++ ECS.Test.Unit/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using NUnit.Framework;
 
 namespace ECS.Test.Unit
 {
@@ -10,6 +11,7 @@ namespace ECS.Test.Unit
     {
         static void Main(string[] args)
         {
+
         }
     }
 }
